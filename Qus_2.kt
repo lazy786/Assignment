6ea31193e1a62aed40c2ffa6076d8567ddd966cd
lazy786 <bikashsahoo67235@gmail.com>
2025-06-25 22:55:47 +0530

    Qus_2.kt

--- Qus_2.kt
+++ Qus_2.kt
@@ -1,1 +1,16 @@
+fun calculateTotalPrice(quantity: Int, pricePerCup: Double?): Double {
+    return if (pricePerCup != null) {
+        quantity * pricePerCup
+    } else {
+        0.0 // Default value if price isn't available
+    }
+}
 
+fun main() {
+    val quantity = 3
+    val pricePerCup: Double? = 4.5
+
+    val totalPrice = calculateTotalPrice(quantity, pricePerCup)
+
+    println("Total price for $quantity cups of coffee: ₹$totalPrice")
+}
